ac75a9b6db0f0c213aa79746a5d4c52505cdd6de
SteffenLm <steffen.merk@sap.com>
2019-08-05 08:01:32 +0200

    Added DB Design

--- db/db_structure.sql
+++ db/db_structure.sql
@@ -4,6 +4,7 @@ USE cego;
 CREATE TABLE user(
 	uid INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL UNIQUE,
-    password VARCHAR(51) NOT NULL,
-    email VARCHAR(60) UNIQUE
+    password VARCHAR(64) NOT NULL,
+    email VARCHAR(60) UNIQUE,
+    jwtkey varchar(32)
 );
