776e659fbff2f1e70f2d02133da5597def0671af
Merk <steffen.merk@sap.com>
2019-08-26 15:03:03 +0200

    Extended Database Schema

---
+++ db/db_mockdata.sql
@@ -0,0 +1,18 @@
+INSERT INTO users (username, password) VALUES ( 'steffenlm', '487A982E3F5FA1DD9F21DF5DC292FF226032388EC9D88ABE6C27C1A4BEF1D159');
+INSERT INTO users (username, password) VALUES ( 'peter', '487A982E3F5FA1DD9F21DF5DC292FF226032388EC9D88ABE6C27C1A4BEF1D159');
+INSERT INTO users (username, password) VALUES ( 'yanner', '487A982E3F5FA1DD9F21DF5DC292FF226032388EC9D88ABE6C27C1A4BEF1D159');
+INSERT INTO users (username, password) VALUES ( 'lader', '487A982E3F5FA1DD9F21DF5DC292FF226032388EC9D88ABE6C27C1A4BEF1D159');
+INSERT INTO users (username, password) VALUES ( 'hanni', '487A982E3F5FA1DD9F21DF5DC292FF226032388EC9D88ABE6C27C1A4BEF1D159');
+INSERT INTO users (username, password) VALUES ( 'dennis', '487A982E3F5FA1DD9F21DF5DC292FF226032388EC9D88ABE6C27C1A4BEF1D159');
+INSERT INTO users (username, password) VALUES ( 'mayer', '487A982E3F5FA1DD9F21DF5DC292FF226032388EC9D88ABE6C27C1A4BEF1D159');
+
+INSERT INTO games (name, playerOne, playerTwo, playerThree, playerFour) 
+VALUES ( 'Der Geile Cego Shit', 1, 2, 3, 5);
+INSERT INTO games (name, playerOne, playerTwo, playerThree, playerFour) 
+VALUES ( 'Der Geile Cego Shit', 1, 4, 5, 7);
+INSERT INTO games (name, playerOne, playerTwo, playerThree, playerFour) 
+VALUES ( 'Der Geile Cego Shit', 1, 3, 2, 5);
+INSERT INTO games (name, playerOne, playerTwo, playerThree, playerFour) 
+VALUES ( 'Der Geile Cego Shit', 6, 3, 2, 5);
+INSERT INTO games (name, playerOne, playerTwo, playerThree, playerFour) 
+VALUES ( 'Der Geile Cego Shit', 7, 5, 3, 1);
--- db/db_structure.sql
+++ db/db_structure.sql
@@ -1,10 +1,32 @@
 CREATE DATABASE cego;
 USE cego;
 
-CREATE TABLE user(
+CREATE TABLE users(
 	uid INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30) NOT NULL UNIQUE,
     password VARCHAR(64) NOT NULL,
     email VARCHAR(60) UNIQUE,
     jwtkey varchar(32)
 );
+
+CREATE TABLE games(
+    gid INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    created DATETIME  DEFAULT CURRENT_TIMESTAMP,
+    playerOne INT NOT NULL,
+    playerTwo INT NOT NULL,
+    playerThree INT NOT NULL,
+    playerFour INT NOT NULL,
+    FOREIGN KEY (playerOne) REFERENCES users (uid),
+    FOREIGN KEY (playerTwo) REFERENCES users (uid),
+    FOREIGN KEY (playerThree) REFERENCES users (uid),
+    FOREIGN KEY (playerFour) REFERENCES users (uid),
+    CONSTRAINT CHK_Players CHECK (
+        playerOne != playerTwo AND
+        playerOne != playerThree AND
+        playerOne != playerFour AND
+        playerTwo != playerThree AND 
+        playerTwo != playerFour AND
+        playerThree != playerFour)
+);
+
