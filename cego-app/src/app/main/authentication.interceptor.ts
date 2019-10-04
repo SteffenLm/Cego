541f6e46f84e20e4fdf081a887affa902c416a03
Merk <steffen.merk@sap.com>
2019-10-04 19:01:09 +0200

    Fixed Progressbar behavior

--- cego-app/src/app/main/authentication.interceptor.ts
+++ cego-app/src/app/main/authentication.interceptor.ts
@@ -4,16 +4,18 @@ import { HttpRequest, HttpInterceptor, HttpHandler, HttpEvent, HttpResponse, Htt
 import { Observable, throwError } from 'rxjs';
 import { LoginService } from '../core/login/login.service';
 import { retry, catchError } from 'rxjs/operators';
+import { MainService } from './main.service';
 
 @Injectable()
 export class AuthenticationInterceptor implements HttpInterceptor {
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private mainService: MainService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       retry(2),
       catchError((error: HttpErrorResponse) => {
+        this.mainService.endLoading();
         if (error.status === 401) {
           this.loginService.logout(true);
         }
