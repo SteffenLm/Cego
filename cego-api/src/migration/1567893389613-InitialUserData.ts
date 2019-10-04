620a74ebef96d339cfd820919150ffb7637b84cd
Merk <steffen.merk@sap.com>
2019-10-04 19:00:35 +0200

    Added More Test Data

--- cego-api/src/migration/1567893389613-InitialUserData.ts
+++ cego-api/src/migration/1567893389613-InitialUserData.ts
@@ -4,13 +4,15 @@ export class InitialUserData1567893389613 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         const queries: string[] = [
-            `INSERT INTO user (id, username, password) VALUES (1, 'steffenlm', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
+            `INSERT INTO user (id, username, password) VALUES (1, 'steffen', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
             `INSERT INTO user (id, username, password) VALUES (2, 'peter', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
             `INSERT INTO user (id, username, password) VALUES (3, 'yanner', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
             `INSERT INTO user (id, username, password) VALUES (4, 'lader', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
             `INSERT INTO user (id, username, password) VALUES (5, 'hanni', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
             `INSERT INTO user (id, username, password) VALUES (6, 'dennis', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
-            `INSERT INTO user (id, username, password) VALUES (7, 'mayer', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`
+            `INSERT INTO user (id, username, password) VALUES (7, 'mayer', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
+            `INSERT INTO user (id, username, password) VALUES (8, 'manu', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`,
+            `INSERT INTO user (id, username, password) VALUES (8, 'alu', '$2b$12$yGbuMfZF86KcsfMvhBbKouADeWg67hO/gZPsbc88LwRc7kzB48yT6')`
         ];
         queries.forEach(query => {
             queryRunner.query(query);
--- cego-api/src/migration/1568224573214-SampleGameDataUserData.ts
+++ cego-api/src/migration/1568224573214-SampleGameDataUserData.ts
@@ -4,11 +4,11 @@ export class SampleGameDataUserData1568224573214 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         const queries: string[] = [
-            `INSERT INTO game(id, name, creatorid) VALUES(999, 'DER GEILE CEGO SHIT',  1)`,
-            `INSERT INTO game_players_user VALUES (999,1)`,
-            `INSERT INTO game_players_user VALUES (999,2)`,
-            `INSERT INTO game_players_user VALUES (999,3)`,
-            `INSERT INTO game_players_user VALUES (999,4)`
+            `INSERT INTO game(id, name, creatorid) VALUES(1, 'DER GEILE CEGO SHIT',  1)`,
+            `INSERT INTO game_players_user VALUES (1,1)`,
+            `INSERT INTO game_players_user VALUES (1,2)`,
+            `INSERT INTO game_players_user VALUES (1,3)`,
+            `INSERT INTO game_players_user VALUES (1,4)`
         ];
         queries.forEach(query => {
             queryRunner.query(query);
@@ -16,8 +16,8 @@ export class SampleGameDataUserData1568224573214 implements MigrationInterface {
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
-        queryRunner.query('DELETE FROM game_players_user WHERE gameId = 999');
-        queryRunner.query('DELETE FROM game WHERE id = 999');
+        queryRunner.query('DELETE FROM game_players_user WHERE gameId = 1');
+        queryRunner.query('DELETE FROM game WHERE id = 1');
 
     }
 
