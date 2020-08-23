ee510f2a7e266de60ece04e8a081666f9d52a216
SteffenLm <steffenlm@outlook.com>
2020-08-23 14:01:06 +0200

    Refactored AppComponent

--- cego-count/src/app/app.component.ts
+++ cego-count/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<app-main></app-main>',
+  styles: ['']
 })
 export class AppComponent {
   title = 'cego-count';
